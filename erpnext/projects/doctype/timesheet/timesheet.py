34774a98e025261cb8d3bd20236bb24395fc2dc4
Huy-Thai <huythai.dev@gmail.com>
2023-09-12 14:13:29 +0700

    update func handle insert timsheets

--- erpnext/projects/doctype/timesheet/timesheet.py
+++ erpnext/projects/doctype/timesheet/timesheet.py
@@ -584,6 +584,7 @@ async def handler_insert_timesheets():
             time_sheet_doc.company = "ACONS"
             time_sheet_doc.employee = emp_name
             time_sheet_doc.status = TIME_SHEET_STATUS[task_status]
+            time_sheet_doc.total_hours = 0.0
             total_hours = 0.0
 
             if len(dates) > 0:
