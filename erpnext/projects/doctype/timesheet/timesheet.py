33bcab95c566b725283a5a7636e6c7512fa1a38a
Huy-Thai <huythai.dev@gmail.com>
2023-09-12 14:09:53 +0700

    update func handle insert timsheets

--- erpnext/projects/doctype/timesheet/timesheet.py
+++ erpnext/projects/doctype/timesheet/timesheet.py
@@ -584,7 +584,7 @@ async def handler_insert_timesheets():
             time_sheet_doc.company = "ACONS"
             time_sheet_doc.employee = emp_name
             time_sheet_doc.status = TIME_SHEET_STATUS[task_status]
-            time_sheet_doc.total_hours = 0.0
+            total_hours = 0.0
 
             if len(dates) > 0:
                 prev_time_logs = time_sheet_doc.time_logs
@@ -596,7 +596,7 @@ async def handler_insert_timesheets():
 
                 for date, hrs in dates.items():
                     curr_log = next((row for row in prev_time_logs if convert_date_to_datetime(row.from_time) == date), None)
-                    time_sheet_doc.total_hours += flt(hrs)
+                    total_hours += flt(hrs)
 
                     if curr_log is None:
                         time_sheet_doc.append(
@@ -618,6 +618,8 @@ async def handler_insert_timesheets():
                     curr_log.task = task_doc.name
                     curr_log.completed = task_status == "Completed"
 
+            print("total_hours", total_hours)
+            time_sheet_doc.total_hours = total_hours
             time_sheet_doc.insert() if is_new_time_sheet else time_sheet_doc.save()              
             if task_status == "Completed": time_sheet_doc.submit()
             excel_data_update[row_num] = f"{new_hash_key}--{time_sheet_doc.name}"
