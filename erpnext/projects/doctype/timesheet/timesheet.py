868d8f782fce75952426d36131cd6e8c3f36a76b
Huy-Thai <huythai.dev@gmail.com>
2023-09-27 11:14:56 +0700

    update func validate_mandatory_fields of doctype timesheet

--- erpnext/projects/doctype/timesheet/timesheet.py
+++ erpnext/projects/doctype/timesheet/timesheet.py
@@ -110,14 +110,16 @@ class Timesheet(Document):
 
 	def validate_mandatory_fields(self):
 		for data in self.time_logs:
-			if not data.from_time and not data.to_time:
-				frappe.throw(_("Row {0}: From Time and To Time is mandatory.").format(data.idx))
+			# TODO: after import all data need open command here
+			# if not data.from_time and not data.to_time:
+			# 	frappe.throw(_("Row {0}: From Time and To Time is mandatory.").format(data.idx))
 
 			if not data.activity_type and self.employee:
 				frappe.throw(_("Row {0}: Activity Type is mandatory.").format(data.idx))
 
-			if flt(data.hours) == 0.0:
-				frappe.throw(_("Row {0}: Hours value must be greater than zero.").format(data.idx))
+			# TODO: after import all data need open command here
+			# if flt(data.hours) == 0.0:
+			# 	frappe.throw(_("Row {0}: Hours value must be greater than zero.").format(data.idx))
 
 	def update_task_and_project(self):
 		tasks, projects = [], []
