5640bb9f2c47a110476783fcde48c065fcddbb51
Huy-Thai <huythai.dev@gmail.com>
2023-11-10 10:21:34 +0700

    update func process handle timesheet from excel

--- erpnext/projects/doctype/timesheet/timesheet.py
+++ erpnext/projects/doctype/timesheet/timesheet.py
@@ -561,14 +561,15 @@ async def handler_insert_timesheets(body_query, num_start, num_end, date_row_num
             pre_time_sheet_doc = frappe.get_doc(doctype = "Timesheet", name = time_sheet_id, employee = emp_name)
             time_sheet_doc = frappe.new_doc("Timesheet")
 			
-            print("status: ", pre_time_sheet_doc.docstatus)
+            print("name: ", pre_time_sheet_doc.name)
+            print("status: ", pre_time_sheet_doc.status)
+            print("docstatus: ", pre_time_sheet_doc.docstatus)
 
             if pre_time_sheet_doc is not None and pre_time_sheet_doc.status == "Submitted":
-                pre_time_sheet_doc.reload()
                 pre_time_sheet_doc.cancel()
     
             if pre_time_sheet_doc is not None and pre_time_sheet_doc.status != "Submitted":
-                pre_time_sheet_doc.reload()
+                # pre_time_sheet_doc.reload()
                 time_sheet_doc = pre_time_sheet_doc
 
             if emp_name is not None:
