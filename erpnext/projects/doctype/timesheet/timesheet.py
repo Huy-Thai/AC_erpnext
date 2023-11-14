c32fd046a5d89d9ce2266926bbf367b4a61f1563
Huy-Thai <huythai.dev@gmail.com>
2023-11-14 08:39:56 +0700

    update func process handle timesheet from excel

--- erpnext/projects/doctype/timesheet/timesheet.py
+++ erpnext/projects/doctype/timesheet/timesheet.py
@@ -556,13 +556,7 @@ async def handler_insert_timesheets(body_query, num_start, num_end, date_row_num
 				}, ["name"])
 
             if parent_task is not None and cell["B"] == "P":
-                parent_task = process_handle_parent_task_by_excel(
-                    project_code,
-					parent_task,
-                    ms_access_token,
-                    body_query,
-                    ParentTaskModel(row_num, cell),
-                )
+                parent_task = process_handle_parent_task_by_excel(parent_task, ms_access_token, body_query, ParentTaskModel(row_num, cell))
 			
             if employee_name == "": continue
             new_key = f"{project_code};{parent_task};{employee_name};{progress};{activity_code};{task};{date_string}"
