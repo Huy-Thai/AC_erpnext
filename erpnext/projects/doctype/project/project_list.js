990074c9422a56c938cd280a0c10ac371b7effe6
Huy-Thai <huythai.dev@gmail.com>
2023-12-13 17:20:21 +0700

    update doctype project

--- erpnext/projects/doctype/project/project_list.js
+++ erpnext/projects/doctype/project/project_list.js
@@ -1,9 +1,9 @@
 frappe.listview_settings['Project'] = {
 	add_fields: ["status", "priority", "is_active", "percent_complete", "expected_end_date", "project_name"],
-	filters:[["status","=", "Open"]],
+	filters:[["status","=", "Opened"]],
 	get_indicator: function(doc) {
-		if(doc.status=="Open" && doc.percent_complete) {
-			return [__("{0}%", [cint(doc.percent_complete)]), "orange", "percent_complete,>,0|status,=,Open"];
+		if(doc.status=="Opened" && doc.percent_complete) {
+			return [__("{0}%", [cint(doc.percent_complete)]), "orange", "percent_complete,>,0|status,=,Opened"];
 		} else {
 			return [__(doc.status), frappe.utils.guess_colour(doc.status), "status,=," + doc.status];
 		}
