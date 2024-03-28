8d7d4b246dc4a3a9dbdec98c8bd7e8b7c40ac5fc
Huy-Thai <huythai.dev@gmail.com>
2024-03-28 11:55:01 +0700

    hot fix data

--- erpnext/projects/doctype/project/project.py
+++ erpnext/projects/doctype/project/project.py
@@ -134,7 +134,6 @@ class Project(Document):
 				subject=task_details.subject,
 				project=self.name,
 				status="Open",
-				priority = "Medium",
 				# exp_start_date=self.calculate_start_date(task_details),
 				# exp_end_date=self.calculate_end_date(task_details),
 				description=task_details.description,
