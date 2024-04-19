99f41c0a6e3a2474057a030290c67647d13c73c6
Huy-Thai <huythai.dev@gmail.com>
2024-04-19 11:59:03 +0700

    update cron job

--- erpnext/projects/doctype/timesheet/timesheet.py
+++ erpnext/projects/doctype/timesheet/timesheet.py
@@ -723,12 +723,20 @@ async def handle_timesheet_file(worksheet_name, url_file, range_start, range_end
     ignore_values = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P"]
     row_date = {k: v for k, v in date_values.items() if k not in ignore_values}
 
-    promises = []
-    for num in range(range_start, range_end):
-        promise = asyncio.ensure_future(handle_single_row(ggSheet, num, row_date, company))
-        promises.append(promise)
+    # promises = []
+    # for num in range(range_start, range_end):
+    #     promise = asyncio.ensure_future(handle_single_row(ggSheet, num, row_date, company))
+    #     promises.append(promise)
+    # cell_values = await asyncio.gather(*promises)
 
-    cell_values = await asyncio.gather(*promises)
+    tasks = []
+    async with asyncio.TaskGroup() as tg:
+        for num in range(range_start, range_end):
+            task = tg.create_task(self.get_values_with_excel_style(num_of_row=num, seed=1, is_return_num=True))
+            tasks.append(task)
+
+    results = await asyncio.wait_for(tasks, timeout=1000)
+    cell_values = [task.result() for task in results]
     cell_list = await worksheet.range(f'A{range_start}:A{range_end}')
     for i, val in enumerate(cell_values):
         if  val is None: continue
