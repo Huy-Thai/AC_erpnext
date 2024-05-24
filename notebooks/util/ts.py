535b50d80f6a2b27672662d6c16f79e923a7523a
nghiach <chnghia@gmail.com>
2024-05-24 14:55:44 +0700

    add notebooks

---
+++ notebooks/util/ts.py
@@ -0,0 +1,112 @@
+class Ts():
+    project_doc_list = {}
+    task_doc_list = {}
+    employee_doc_list = {}
+    timesheet_doc_list = {}
+    conn = None
+
+    def __init__(self, conn):
+        self.conn = conn
+        
+    def get_project_doc(self, doc_name):
+        # return if cached else get db & cache
+        if self.project_doc_list.get(doc_name):
+            return self.project_doc_list[doc_name]
+        else:
+            doc = self.conn.get_doc('Project', doc_name)
+            if doc:
+                self.project_doc_list[doc_name] = doc
+                return doc
+    
+        return None
+    
+    def get_employee_doc(self, doc_name):
+        # return if cached else get db & cache
+        if self.employee_doc_list.get(doc_name):
+            return self.employee_doc_list[doc_name]
+        else:
+            doc = self.conn.get_doc('Employee', doc_name)
+            if doc:
+                self.employee_doc_list[doc_name] = doc
+                return doc
+        return None
+    
+    def get_employee_doc_by_name(self, employee_name):
+        doc_name = self.conn.get_value("Employee", 
+                                  ["name"],
+                                  [["employee_name", "=", employee_name]])
+        print(f"employee: {doc_name}")
+        if doc_name:
+            return self.get_employee_doc(doc_name['name'])
+            
+        return None
+    
+    def get_task(self, doc_name):
+        # return if cached else get db & cache
+        if self.task_doc_list.get(doc_name):
+            return self.task_doc_list[doc_name]
+        else:
+            doc = self.conn.get_doc('Task', doc_name)
+            if doc:
+                self.task_doc_list[doc_name] = doc
+                return doc
+        return None
+
+    def get_task_doc_by_fields(self, payload={}):
+
+        if (payload["subject"] == "" or
+            payload["project_code"] == "" or
+            payload["phase_name"] == ""):
+            print(f"Cannot get task doc due fields empty")
+            return None
+        
+    
+        filters = [["subject", "=", payload["subject"]],
+                   ["project", "=", payload["project_code"]], 
+                   ["type", "=", payload["phase_name"]],
+                   ["is_group", "=", payload["is_group"]]]
+        if payload.get("parent_task"):
+            filters.append(["parent_task", "=", payload["parent_task"]])
+        
+        doc_name = self.conn.get_value("Task", ["name"], filters)
+        if doc_name:
+            # print(f"doc_name existed")
+            return self.get_task(doc_name["name"])
+        return None
+
+    def get_timesheet(self, doc_name):
+        # return if cached else get db & cache
+        if self.timesheet_doc_list.get(doc_name):
+            return self.timesheet_doc_list[doc_name]
+        else:
+            doc = self.conn.get_doc('Timesheet', doc_name)
+            if doc:
+                self.timesheet_doc_list[doc_name] = doc
+                return doc
+        return None
+
+    def get_timesheet_name_by_fields(self, payload={}):
+        doc_name = self.conn.get_value("Timesheet", ["name"],
+                                      [["employee", "=", payload['employee_code']],
+                                       ["parent_project", "=", payload['project_code']],
+                                       ["custom_task", "=", payload['task']],
+                                       ["custom_phase", "=", payload['phase_name']]])
+        
+        return doc_name
+
+    def create_task(self, payload = {}):
+        doc = {
+            "doctype": "Task"
+        }
+        doc["subject"] = payload["subject"]
+        doc["project"] = payload["project_code"]
+        doc["type"] = payload["phase_name"]
+        doc["is_group"] = payload["is_group"]
+        if payload.get("parent_task"):
+            doc["parent_task"] = payload["parent_task"]
+        
+        return self.conn.insert(doc)
+        # pass
+
+    def create_timesheet(self, payload):
+        self.conn.insert(payload)
---
+++ notebooks/util/util.py
@@ -0,0 +1,116 @@
+import os
+import json
+import hashlib
+from dateutil import parser
+from datetime import datetime
+
+EXCEL_TASK_PRIORITY = {
+    "": "Medium",
+    "1_Urgen": "Urgent",
+    "2_Important": "High",
+    "3_Medium": "Medium",
+    "4_Low": "Low",
+}
+EXCEL_TYPE_PARENT_TASK = {
+    "": "Other",
+    "0_Pre CO": "Pre Concept",
+    "1_CO": "Concept",
+    "2_BD": "Basic Design",
+    "3_DD": "Design Development",
+    "4_TD": "Tender Doc",
+    "5_CD": "Construction",
+    "6_AU": "Authorship",
+    "7_Other": "Other",
+}
+EXCEL_TASK_STATUS = {
+    "": "Open",
+    "1_Open": "Open",
+    "2_In progress": "In Progress",
+    "3_Pending": "Pending",
+    "4_Cancel": "Cancel",
+    "5_Done": "Done",
+    "6_Review": "Review",
+}
+EXCEL_TIME_SHEET_STATUS = {
+    "": "Draft",
+    "Open": "Draft",
+    "In Progress": "Draft",
+    "Pending": "Draft",
+    "Review": "Draft",
+    "Done": "Submitted",
+    "Cancel": "Cancelled",
+}
+EXCEL_TIME_SHEET_DOC_STATUS = {
+    "": 0,
+    "Draft": 0,
+    "Submitted": 1,
+    "Cancelled": 2,
+}
+
+def mapping_row_with_attr(row_data):
+    attrs = {}
+    # no need anymore
+    attrs["import_key"]          = row_data["A"] if "A" in row_data else ""
+    # import_key based on
+    # task
+    # new_key = f"{payload.expected_start_date};{payload.expected_end_date};{payload.new_end_date}"
+    # new_hash_key = hash_str_8_dig(new_key)
+    # prev_hash_key, _, __ = split_str_get_key(input_data=payload.prev_hash_key, char_split = "--")
+    # timesheet
+    # new_key = f"{project_code};{parent_task};{employee_name};{progress};{activity_code};{task};{excel_task_status};{date_string}"
+    # new_hash_key = hash_str_8_dig(new_key)
+    # A_column_key = f"{new_hash_key}--{task_doc}--{new_time_sheet_doc.name}"
+    # prev_hash_key, task_id, time_sheet_id = split_str_get_key(input_data=cell["A"], char_split="--")
+    
+    attrs["row_type"]            = row_data["B"] if "B" in row_data else ""
+    attrs["project_code"]        = row_data["C"] if "C" in row_data else ""
+    attrs["project_name"]        = row_data["D"] if "D" in row_data else ""
+
+    
+    attrs["expected_start_date"] = parser.parse(row_data["E"]) if "E" in row_data and row_data["E"] != "" else None
+    attrs["expected_end_date"]   = parser.parse(row_data["F"]) if "F" in row_data and row_data["F"] != "" else None
+    attrs["new_end_date"]        = parser.parse(row_data["G"]) if "G" in row_data and row_data["G"] != "" else None
+    
+    attrs["phase_name"]          = EXCEL_TYPE_PARENT_TASK[row_data["H"] if "H" in row_data else ""]
+    # attrs["phase_name"           = EXCEL_TYPE_PARENT_TASK[row_data["H"] if "H" in row_data else ""]
+    # expected_time = float(cell["I"]) if "I" in cell and cell["I"] != "" else 0.0
+    attrs["expected_time"]       = float(row_data["I"]) if "I" in row_data and row_data["I"] != "" else 0.0
+    # col_J: real time (sum)
+    attrs["task_priority"]       = EXCEL_TASK_PRIORITY[row_data["K"] if "K" in row_data else ""]
+    attrs["progress"]            = row_data["L"].replace("%", "") if "L" in row_data else ""
+    attrs["employee_name"]       = row_data["M"] if "M" in row_data else ""
+    attrs["activity_code"]       = row_data["N"] if "N" in row_data else ""
+    if attrs["activity_code"] == "":
+        attrs["activity_code"] = "0000"
+    attrs["task"]                = row_data["O"] if "O" in row_data else ""
+    attrs["subject"]             = row_data["O"] if "O" in row_data else ""
+    if attrs["subject"] == "":
+        attrs["subject"] = "Other"
+    attrs["excel_task_status"]   = EXCEL_TASK_STATUS[row_data["P"] if "P" in row_data else ""]
+    attrs["task_status"]         = EXCEL_TASK_STATUS[row_data["P"] if "P" in row_data else ""]
+    # self.task_number = num
+    
+    return attrs
+
+def mapping_row_with_time_log(row_data, row_date_header):
+    new_date = {}
+    date_string = ""
+    for column, value in row_data.items():
+        if column in row_date_header and value != None and value != "":
+            date = parser.parse(row_date_header[column])
+            new_date[date] = value
+            date_string = date_string + column + "-" + value + ";"
+    return new_date, date_string
+
+def is_row_empty(row_attrs):
+    if row_attrs['project_code'] == "":
+        return True
+    return False
+
+def is_row_timesheet_empty(row_attrs):
+    if row_attrs['project_code'] == "":
+        return True
+    return False
+
+def is_time_log_row_empty(row_time_logs):
+    return True
